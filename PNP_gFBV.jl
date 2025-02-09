e765c1297cc51247fcadf3fee18cca9070f3c933
ArunKamath8 <92821531+ArunKamath8@users.noreply.github.com>
2025-02-09 15:05:02 -0800

    Update PNP_gFBV.jl
    vectorized derivates to make faster

--- PNP_gFBV.jl
+++ PNP_gFBV.jl
@@ -4,29 +4,31 @@ using Dates
 using JSON
 
 #setting up plot
-fig, ax = subplots(figsize=(7,7)) 
+fig, ax = subplots(3, 1, sharex=true, figsize=(5,7)) 
 
-#defining parameters
-num_spaces = 1000
-num_time_steps = 10^6
-dt = 1 / num_time_steps
+#GLOBAL VARIABLES
+num_spaces = 200
+dt = 10^-4
+char_times = 1
+num_time_steps = char_times / dt
 grid = collect(LinRange(-1, 1, num_spaces+1))
-dx = (2 / (num_spaces))
-filename="no_reaction"
+dx = grid[2] - grid[1]
+filename="no_rxn"
 
-#physical parameters
+#PHYSICAL PARAMS
 epsilon = 0.05
 delta = 0.1
-v = 2
+v = 0.5
 D = 2.5678313e-10
-L = 1e-4
-kf = 2.5 * 0
-kb = 0.75 * 0
+L = 1e-5
+kf = 0
+kb = 0
 Cb = 0.150
+C_solid_Li = 77
 
 parameters_dict = Dict(
     "num_spaces" => num_spaces, 
-    "num_time_steps" => num_time_steps, 
+    "num__time_steps" => num_time_steps, 
     "dt" => dt, 
     "epsilon" => epsilon, 
     "delta" => delta, 
@@ -38,30 +40,12 @@ parameters_dict = Dict(
 
 #defining central difference for laplacian
 function deriv2(var)   
-    derivative = []
-    for i in 1:length(var)
-        if i == 1 || i == num_spaces+1
-            push!(derivative, 0.0)
-        else
-            push!(derivative, (var[i + 1] - 2 * var[i] + var[i-1])/(dx^2))
-        end
-    end
-    return derivative
+    return (var[3:end] - 2 * var[2:end-1] + var[1:end-2]) / (dx^2)
 end
 
 #defining central difference for gradient
 function deriv(var)
-    derivative = []
-    for i in 1:length(var)
-        if i == 1
-            push!(derivative, (var[2] - var[1])/dx)
-        elseif i == num_spaces+1
-            push!(derivative, (var[num_spaces+1] - var[num_spaces])/dx)
-        else
-            push!(derivative, (var[i+1]-var[i-1])/(2*dx))
-        end
-    end
-    return derivative
+    return (var[3:end] - var[1:end-2]) / (2*dx)
 end
 
 #initial conditions: uniform concentration and charge neutrality
@@ -69,15 +53,10 @@ c = 0 .* grid .+ 1
 rho = 0 .* grid
 phi = v .* grid
 
-#defining poisson solver matrix: includes robin boundary condition for potential
-dl = push!(ones(num_spaces-1), -delta * epsilon)
-du = pushfirst!(ones(num_spaces-1), -delta * epsilon)
-d = push!(ones(num_spaces-1) .* -2, dx + delta * epsilon)
-pushfirst!(d, dx + delta * epsilon)
-matrix = LinearAlgebra.Tridiagonal(dl, d, du)
-
 #plot initial potential
-plot(grid, phi, label="initial")
+ax[1].plot(grid, phi, label="initial phi")
+ax[2].plot(grid, c+rho, label="initial +")
+ax[3].plot(grid, c-rho, label="initial -")
 Rleft = []
 
 #storing gradients as global variable
@@ -85,10 +64,16 @@ dc = collect(LinRange(-1, 1, num_spaces+1))
 drho = collect(LinRange(-1, 1, num_spaces+1))
 dphi = collect(LinRange(-1, 1, num_spaces+1))
 
+#defining poisson matrix
+dl = push!(ones(num_spaces-1), -delta * epsilon)
+du = pushfirst!(ones(num_spaces-1), -delta * epsilon)
+d = push!(ones(num_spaces-1) .* -2, dx + delta * epsilon)
+pushfirst!(d, dx + delta * epsilon)
+matrix = LinearAlgebra.Tridiagonal(dl, d, du)
+
 for time in 1:num_time_steps
-#enforce boundary conditions
-    #poissons equations for potential
-    soln_vector = (-1 .* rho[2:(num_spaces)] ./ epsilon^2 .* dx^2)
+    #enforce boundary conditions
+    soln_vector = (-1 .* rho[2:end-1] ./ epsilon^2 .* dx^2)
     push!(soln_vector, v * dx)
     pushfirst!(soln_vector, -v * dx)
 
@@ -100,13 +85,14 @@ for time in 1:num_time_steps
     A = exp(-phidrop/2)
     B = exp(phidrop/2)
 
-    BVmatrix = [(2 * kf * A * dx + 1) (2 * kf * A * dx - gradphiboundary*dx); (2 * kf * A * dx - gradphiboundary*dx)  (2 * kf * A * dx + 1)]
+    BVmatrix = [(kf * A * dx + 1) (kf * A * dx - gradphiboundary*dx); (kf * A * dx - gradphiboundary*dx)  (kf * A * dx + 1)]
     BVsoln = [(c[2]+(kb/Cb)*B*dx); (rho[2]+(kb/Cb)*B*dx)]
     BVboundary = BVmatrix \ BVsoln
     c[1] = BVboundary[1]
     rho[1] = BVboundary[2]
 
-    push!(Rleft, 2 * kf * (c[1]+rho[1]) * A - kb/Cb * B)
+    reaction_rate = kf * (c[1]+rho[1]) * A - kb/Cb * B
+    push!(Rleft, reaction_rate)
 
     #BV flux condition at boundary 2
     gradphiboundary = (phi[num_spaces+1] - phi[num_spaces])/dx
@@ -114,7 +100,7 @@ for time in 1:num_time_steps
     A = exp(-phidrop/2)
     B = exp(phidrop/2)
 
-    BVmatrix = [(2 * kf * A * dx + 1) (2 * kf * A * dx + gradphiboundary*dx); (2 * kf * A * dx + gradphiboundary*dx)  (2 * kf * A * dx + 1)]
+    BVmatrix = [(kf * A * dx + 1) (kf * A * dx + gradphiboundary*dx); (kf * A * dx + gradphiboundary*dx)  (kf * A * dx + 1)]
     BVsoln = [(c[num_spaces]+(kb/Cb)*B*dx); (rho[num_spaces]+(kb/Cb)*B*dx)]
     BVboundary = BVmatrix \ BVsoln
     c[num_spaces+1] = BVboundary[1]
@@ -130,20 +116,29 @@ for time in 1:num_time_steps
     grad2phi = deriv2(phi)
     
     #update c and rho; boundary is taken care of at the top of the loop
-    dcdt = epsilon .* (grad2c .+ drho .* dphi .+ rho .* grad2phi)
-    drhodt = epsilon .* (grad2rho .+ dc .* dphi .+ c .* grad2phi)
-    c[2:num_spaces] .+= (dt .* dcdt[2:num_spaces])
-    rho[2:num_spaces] .+= (dt .* drhodt[2:num_spaces])
+    dcdt = epsilon .* (grad2c .+ drho .* dphi .+ rho[2:end-1] .* grad2phi)
+    drhodt = epsilon .* (grad2rho .+ dc .* dphi .+ c[2:end-1] .* grad2phi)
+    c[2:end-1] .+= (dt .* dcdt)
+    rho[2:end-1] .+= (dt .* drhodt)
 
     if time in [num_time_steps/5 2*num_time_steps/5 3*num_time_steps/5 4*num_time_steps/5 num_time_steps] 
-        #plt.plot(grid, rho, label="rho")
-        plt.scatter(grid, phi, label="phi @ " * string(time*dt), s=0.5)
-        #plt.plot(grid, c+rho, label="cation") 
+        ax[1].plot(grid, phi, label="phi @ t =" * string(time*dt))
+        ax[2].plot(grid, c+rho, label="cation @ t =" * string(time*dt)) 
+        ax[3].plot(grid, c-rho, label="anion @ t =" * string(time*dt))
     end
 end
-plt.legend()   
+
+#plotting and labeling
+ax[3].set_xlabel("Dimensionless position")    
+ax[1].set_ylabel("potential")    
+ax[2].set_ylabel("cation concentration")    
+ax[3].set_ylabel("anion concentration")    
+ax[1].legend(loc="center left", bbox_to_anchor=(0.9, 0.5))   
+ax[2].legend(loc="center left", bbox_to_anchor=(0.9, 0.5))   
+ax[3].legend(loc="center left", bbox_to_anchor=(0.9, 0.5))   
 savefig("/Users/arun/Desktop/BVPlots/" * filename, dpi=300)
 
+#dump parameters
 open("/Users/arun/Desktop/BVPlots/" * filename * ".json", "w") do file
     JSON.print(file, parameters_dict)
 end
